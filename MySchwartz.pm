75c9eee71ad910d4afd0cdd60fb88a471de25cd3
Stash <stash@aedion.net>
2009-05-14 02:43:35 -0700

    Make the connection verbose

--- MySchwartz.pm
+++ MySchwartz.pm
@@ -4,19 +4,25 @@ use DBD::Pg;
 use TheSchwartz::Moosified;
 
 extends 'TheSchwartz::Moosified';
+has '+databases' => ( default => \&connect );
+has '+verbose' => ( default => 1 );
 
-my $dsn = "dbi:Pg:database=vanpm";
+our $dsn = "dbi:Pg:database=vanpm";
+our $DBH;
 
-our $DBH = DBI->connect($dsn, $ENV{USER}, "",  {
-    AutoCommit => 1,
-    pg_enable_utf8 => 1,
-    PrintError => 0,
-    RaiseError => 0,
-});
-
-has '+databases' => (
-    default => sub { [ $DBH ] },
-);
+sub connect {
+    return [$DBH] if $DBH;
+    my $dbh = DBI->connect($dsn, $ENV{USER}, "",  {
+        AutoCommit => 1,
+        pg_enable_utf8 => 1,
+        PrintError => 0,
+        RaiseError => 0,
+    });
+    print "Connected to DB\n" if $dbh;
+    die "Can't connect to DB\n" unless $dbh;
+    $DBH = $dbh;
+    return [$DBH];
+}
 
 no Moose;
 1;
