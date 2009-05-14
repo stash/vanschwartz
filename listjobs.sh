374a69cb02b4aa49bb0717564fdd757f67b5a39f
Stash <stash@aedion.net>
2009-05-14 02:45:56 -0700

    set PGDATABASE for shell scripts

--- listjobs.sh
+++ listjobs.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+PGDATABASE=vanpm
 psql-8.2 -c 'select jobid,funcid,coalesce,run_after,grabbed_until from job'
--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -x
-
-dropdb-8.2 vanpm
-createdb-8.2 -E UTF-8 -O stash vanpm
+PGDATABASE=vanpm
+dropdb-8.2 $PGDATABASE
+createdb-8.2 -E UTF-8 -O stash $PGDATABASE
 psql-8.2 -f schwartz.sql
